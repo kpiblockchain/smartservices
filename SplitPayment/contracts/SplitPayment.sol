a79c8296d842cf548c2ba622800ae211e598116c
Jakub Tomkiewicz <jakub.tomkiewicz@itmagination.com>
2017-11-08 18:44:43 +0100

    Dodanie obsługi opłaty i definiowania wielu wykonawców na raz.

--- SplitPayment/contracts/SplitPayment.sol
+++ SplitPayment/contracts/SplitPayment.sol
@@ -1,19 +1,21 @@
 pragma solidity ^0.4.0;
+
 /// Kontrakt per zasób
 contract SplitPayment {
     
     struct Shareholder {
         address account; // adres konta
-        uint256 share; // waga
+        uint share; // waga
     }
-    
+
+    uint fee; // opłata, w procentach
     address provider; // adres zarządzający dostępem do zasobu
+    uint sharesSum; // suma wag zasobu
     Shareholder[] shareholders; // adresy mające prawa do zasobu
-    uint256 sharesSum; // suma wag zasobu
-    //bool valid; // zasób może zostać usunięty/wyłączony
     
-    function SplitPayment() public {
-        provider = msg.sender;
+    modifier validateFee(uint feePercent) {
+        require(feePercent < 100);
+        _;
     }
     
     modifier providerOnly() {
@@ -21,22 +23,42 @@ contract SplitPayment {
         _;
     }
     
-    function addOrUpdateShareholder(address account, uint256 share) public providerOnly() {
+    modifier areEqual(uint first, uint second) {
+        require(first == second);
+        _;
+    }
+
+    function SplitPayment(uint feePercent, address[] accounts, uint[] shares) public validateFee(feePercent) {
+        provider = msg.sender;
+        fee = feePercent;
+        addShareholders(accounts, shares);
+    }
+    
+    function addShareholder(address account, uint share) public providerOnly() {
         shareholders.push(Shareholder(account, share));
         sharesSum += share;
     }
     
-    function() payable public {
-        uint256 length = shareholders.length;
-        for (uint i = 0; i < length; i++) {
-            var shareholder = shareholders[i];
-            shareholder.account.transfer(msg.value * (shareholder.share * 1000000 / sharesSum) / 1000000);
+    function addShareholders(address[] accounts, uint[] shares) public providerOnly() areEqual(accounts.length, shares.length) {
+        uint count = accounts.length;
+        for (uint i = 0; i < count; i++) {
+            shareholders.push(Shareholder(accounts[i], shares[i]));
+            sharesSum += shares[i];
         }
     }
     
-    /*function remove(uint256 resourceId) public providerOnly(resourceId) {
-        // todo invalidate resource
-    }*/
+    function() payable public {
+        uint length = shareholders.length;
+        uint sharesSumAfterFee = sharesSum * (100 + fee) / 100;
+        for (uint i = 0; i < length; i++) {
+            var shareholder = shareholders[i];
+            shareholder.account.transfer(msg.value * shareholder.share / sharesSumAfterFee);
+        }
+    }
+
+    function getFee() public providerOnly() {
+        provider.transfer(this.balance);
+    }
     
     function destroyContract() public providerOnly {
         selfdestruct(provider);
